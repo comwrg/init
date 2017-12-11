b97009f301c61857af858d7d1d803040df229a54
comwrg <xcomwrg@gmail.com>
2017-12-11 19:14:18 +0800

    .

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo add-apt-repository ppa:hzwhuang/ss-qt5 -y
 sudo apt-get update
-sudo apt-get install -y git python-pip python3-pip python-dev python3-dev python-tk python3-tk vim chromium-browser shadowsocks-qt5
+sudo apt-get install -y git python-pip python3-pip python-dev python3-dev python-tk python3-tk vim chromium-browser shadowsocks-qt5 build-essential
 
+git config --global user.name comwrg
+git config --global user.email xcomwrg@gmail.com
+git config --global commit.gpgsign true
 
+sudo update-alternatives --config editor
