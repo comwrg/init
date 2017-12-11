64c2e0d5966b6bf824e0050bdc8810b226c62a4a
comwrg <xcomwrg@gmail.com>
2017-12-11 18:50:07 +0800

    .

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo add-apt-repository ppa:hzwhuang/ss-qt5 -y
+sudo apt-get update
+sudo apt-get install -y git python-pip python3-pip python-dev python3-dev python-tk python3-tk vim chromium-browser shadowsocks-qt5
+
+
