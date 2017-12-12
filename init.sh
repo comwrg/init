9acd5616ce08b222e09597a03736c1200f615e6c
comwrg <xcomwrg@gmail.com>
2017-12-12 08:21:50 +0800

    add htop

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository ppa:hzwhuang/ss-qt5 -y
 sudo apt-get update
-sudo apt-get install -y git python-pip python3-pip python-dev python3-dev python-tk python3-tk vim chromium-browser shadowsocks-qt5 build-essential
+sudo apt-get install -y git python-pip python3-pip python-dev python3-dev python-tk python3-tk vim chromium-browser shadowsocks-qt5 build-essential htop
 
 git config --global user.name comwrg
 git config --global user.email xcomwrg@gmail.com
