2b16799d2c30d3cde6afe49dd76aaab8a12403e4
comwrg <xcomwrg@gmail.com>
2017-12-19 16:54:01 +0800

    add screen

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository ppa:hzwhuang/ss-qt5 -y
 sudo apt-get update
-sudo apt-get install -y git python-pip python3-pip python-dev python3-dev python-tk python3-tk vim chromium-browser shadowsocks-qt5 build-essential htop default-jdk tree golang-1.9-go
+sudo apt-get install -y git python-pip python3-pip python-dev python3-dev python-tk python3-tk vim chromium-browser shadowsocks-qt5 build-essential htop default-jdk tree golang-1.9-go screen
 
 sudo cp /usr/lib/go-1.9/bin/go /usr/bin/go -s
 
